0de11e098fbbee8019fd056464088c023e26c359
Doaaxv <35659577+Doaaxv@users.noreply.github.com>
2020-06-02 10:47:56 +0300

    stack throws underflow error

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -13,7 +13,9 @@ const stackFactory = () => {
     push: () => {
       if(count === 2) throw new Error('capacity overflow error')
       count+=1},
-    pop: () =>{count-=1}
+    pop: () =>{
+      if(count===0) throw new Error('capacity underflow error')
+      count-=1}
   }
 };
 
@@ -52,7 +54,9 @@ describe('a stack', () => {
     stack.push()
     expect(()=>{stack.push()}).toThrowError('capacity overflow error')
   });
-  it.todo('throw underflow error when popping an empty stack');
+  it('throw underflow error when popping an empty stack',()=>{
+    expect(()=>{stack.pop()}).toThrowError('capacity underflow error')
+  });
   it.todo('pops the same one item when pushed');
   it.todo('pops two items with the most recent first');
   it.todo('accepts only a positive capacity');
